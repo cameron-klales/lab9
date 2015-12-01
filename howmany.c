be568af54bb85dfdbd13654914c63a93a344c85f
cameron-klales <tuf62129@temple.edu>
2015-12-01 11:47:28 -0500

    Update howmany.c

--- howmany.c
+++ howmany.c
@@ -1,1 +1,15 @@
-
+int howmany(void){
+  int i
+  
+  while(1){
+  scanf("How many questions would you like to answer? (Your options are 3, 4 or 5) %d", i);
+  
+  if(i<3 || i>5){
+    puts("Dumbass");
+    continue;
+  }else{
+    puts("K.");
+    return i;
+  }
+  }
+}
